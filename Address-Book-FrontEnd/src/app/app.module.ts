806e716c02a9408f30a8d987f60983b6712f6726
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-23 23:36:04 +0600

    app module is created

--- Address-Book-FrontEnd/src/app/app.module.ts
+++ Address-Book-FrontEnd/src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { UserRoutingModule } from './users/user-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserModule } from './users/user.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserModule,
+    AppRoutingModule,
+    UserRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
