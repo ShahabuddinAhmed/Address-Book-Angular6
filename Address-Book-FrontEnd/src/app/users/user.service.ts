85242dcd38aa7fcd5323e4272c1db75dbcafbbf8
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-27 20:55:05 +0600

    get method is added

--- Address-Book-FrontEnd/src/app/users/user.service.ts
+++ Address-Book-FrontEnd/src/app/users/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Register } from '../users/models/register';
-import { Http, Headers } from '@angular/http';
+import { Users } from '../users/models/user';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
@@ -9,15 +10,20 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   register(_register: Register) {
     const httpOptions = {
-      headers: new Headers({
+      headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     };
 
     return this.http.post('http://localhost:3000/user/register', _register, httpOptions);
   }
+
+  getAlluser(): Observable<Users[]> {
+    return this.http.get<Users[]>('http://localhost:3000/user/');
+  }
+
 }
