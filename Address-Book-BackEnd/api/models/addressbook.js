436b056974226497a290d7ab0911eb4f4d00b85b
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-28 20:04:27 +0600

    controller is modified

--- Address-Book-BackEnd/api/models/addressbook.js
+++ Address-Book-BackEnd/api/models/addressbook.js
@@ -4,14 +4,13 @@ const addressBooksSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     fullName: { type: String, required: true },
     nickName: { type: String, required: true },
-    phone1: { type: String, required: true, unique: true },
+    phone1: { type: String, required: true },
     phone2: { type: String, required: true },
     address: { type: String, required: true },
     website: { type: String, required: true },
     email: {
         type: String,
         required: true,
-        unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     birthday: { type: String, required: true },
