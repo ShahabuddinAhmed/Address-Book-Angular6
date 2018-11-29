794807ea3a3d39f9713f5f91779af2b63ef2a034
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-11-29 21:41:56 +0600

    service business logic is added

--- Address-Book-FrontEnd/src/app/users/user.service.ts
+++ Address-Book-FrontEnd/src/app/users/user.service.ts
@@ -4,6 +4,7 @@ import { Users } from '../users/models/user';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Login } from './models/login';
 
 @Injectable({
   providedIn: 'root'
@@ -22,8 +23,22 @@ export class UserService {
     return this.http.post('http://localhost:3000/user/register', _register, httpOptions);
   }
 
+  login(_login: Login) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post('http://localhost:3000/user/login', _login, httpOptions);
+  }
+
   getAlluser(): Observable<Users[]> {
     return this.http.get<Users[]>('http://localhost:3000/user/');
+  }
+
+  getToken(token: any) {
+    return localStorage.getItem(token);
   }
 
 }
