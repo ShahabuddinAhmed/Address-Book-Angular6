36d0a8447483548dae40ab7cadb5c451602aa9ea
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-12-02 18:54:16 +0600

    book model is added

---
+++ Address-Book-FrontEnd/src/app/users/models/book.ts
@@ -0,0 +1,11 @@
+export class Book {
+  fullName: string;
+  nickName: string;
+  phone1: string;
+  phone2: string;
+  address: string;
+  website: string;
+  email: string;
+  birthday: string;
+  usersID: string;
+}
