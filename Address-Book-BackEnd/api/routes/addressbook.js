42262ba2f9f836f4b7ab1e1f5d08f74c8063e3e1
ShahabuddinAhmed <shahabuddin.cse.ru@gmail.com>
2018-12-02 18:49:22 +0600

    addressbook route is updated

--- Address-Book-BackEnd/api/routes/addressbook.js
+++ Address-Book-BackEnd/api/routes/addressbook.js
@@ -6,6 +6,7 @@ const addressBookController = require('../controller/addressbook');
 router.get('/',checkAuth, addressBookController.getAllAddressBook);
 router.post('/create',checkAuth, addressBookController.createAddressBook);
 router.get('/:userID', checkAuth, addressBookController.getOneAddressBook);
+router.get('/getone/:userID', checkAuth, addressBookController.updateOneAddressBook);
 router.patch('/update/:userID', checkAuth, addressBookController.updateAddressBook);
 router.delete('/delete/:userID', checkAuth, addressBookController.deteleteAddressBook);
 
